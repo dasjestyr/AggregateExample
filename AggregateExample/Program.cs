f686e5f387c6c9fe38129e2df86abfb77e383442
Jeremy Stafford <draepetan@live.com>
2017-12-26 20:50:20 -0700

    Add project files.

---
+++ AggregateExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AggregateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
