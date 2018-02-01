cf889c70fe2e1c96f40730589b69ee6b1f24c67e
Jeremy Stafford <dasjestyr@gmail.com>
2018-01-31 21:55:38 -0700

    added some comments to repository

--- AggregateExample3/AccountRepository.cs
+++ AggregateExample3/AccountRepository.cs
@@ -9,11 +9,19 @@ namespace AggregateExample3
 
         public Account Get(Guid id)
         {
+            // simplified for brevity. In an ES solution, this would retrieve
+            // the stream and apply each event to the aggregate root. The
+            // aggregate root would proxy any events to subordinate entities
+            // who would then apply the events to themselves in order to 
+            // update state
             return InMemoryStore.ContainsKey(id) ? InMemoryStore[id] : null;
         }
 
         public void Save(Account entity)
         {
+            // this is simplified for brevity. In an ES solution, this
+            // would pull out uncommitted events and persist them to the stream
+
             if (InMemoryStore.ContainsKey(entity.Id))
             {
                 InMemoryStore[entity.Id] = entity;
